d09a2b301243c2d3e2c6cfa30af1a42d59fad68e
reade-contentsquare <64031824+reade-contentsquare@users.noreply.github.com>
2020-07-17 14:01:02 -0700

    Add Contentsquare

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import store from './store';
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
+router.beforeEach((to, from, next) => {
+	window._uxa.push(['trackPageview', to.name]);
+  next();
+})
+
 new Vue({
   router, store,
   render: h => h(App)
