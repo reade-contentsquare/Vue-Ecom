98fbd607f66dfae86a9c55ce7b11da33243bded0
reade-contentsquare <64031824+reade-contentsquare@users.noreply.github.com>
2020-07-23 10:08:57 -0700

    Move puppeteer into seperate directory

--- puppeteer/puppeteer.mjs
+++ puppeteer/puppeteer.mjs
@@ -4,7 +4,7 @@ import * as sessions from './sessions/index.js'
 
 // Run the following command to get a chrome ws address to run locally
 // /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
-const CHROME_WS = 'ws://[::1]:9222/devtools/browser/6aafedf8-b75d-40e2-9eb3-22f2e12bcc9e';
+const CHROME_WS = '';
 const NUM_TIMES = process.env['NUM_TIMES'] || 3;
 
 (async () => {
