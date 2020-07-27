352976b8bb6ca18f6558760e71fa4e00c64e0062
reade-contentsquare <64031824+reade-contentsquare@users.noreply.github.com>
2020-07-27 11:23:36 -0700

    Add mobile and forgot password sessions

--- puppeteer/puppeteer.mjs
+++ puppeteer/puppeteer.mjs
@@ -5,24 +5,27 @@ import * as sessions from './sessions/index.js'
 // Run the following command to get a chrome ws address to run locally
 // /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
 const CHROME_WS = '';
-const NUM_TIMES = process.env['NUM_TIMES'] || 3;
+const NUM_TIMES = process.env['NUM_TIMES'] || 1;
+const CONCURRENT_SESSIONS = process.env['CONCURRENT_SESSIONS'] || 1;
 
 (async () => {
   try {
     const browser = CHROME_WS ? await puppeteer.connect({ browserWSEndpoint: CHROME_WS }) : await puppeteer.launch();
     const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-    progress.start(NUM_TIMES, 0);
+    progress.start(NUM_TIMES * CONCURRENT_SESSIONS, 0);
 
     for (var i = 0; i < NUM_TIMES; i++) {
       let promises = [];
-      for (const name in sessions) {
-        promises.push(sessions[name](browser))
+      for (var j = 0; j < CONCURRENT_SESSIONS; j++) {
+        for (const name in sessions) {
+          promises.push(sessions[name](browser))
+        }
       }
       await Promise.all(promises);
-      progress.update(i + 1);
+      progress.update((i + 1) * CONCURRENT_SESSIONS);
     }
 
-    if (!CHROME_WS) await browser.close();
+    await browser.close();
   } catch(e) {
     console.log("ERROR", e)
   }
--- puppeteer/sessions/index.js
+++ puppeteer/sessions/index.js
@@ -227,3 +227,138 @@ export async function contactUs(browser) {
 
   await page.close();
 }
+
+export async function resetPassword(browser) {
+	const page = await browser.newPage()
+	await page.goto('http://contentsquare-presales-ecomm-web.s3-website-us-west-2.amazonaws.com/')
+
+	await page.setViewport({ width: 1440, height: 766 })
+
+	await page.waitForSelector('#app > .container-flex > .navbar > .user > h5')
+	await page.click('#app > .container-flex > .navbar > .user > h5')
+	await delay(DELAY);
+
+	await page.waitForSelector('.modal-content > .modal-body > .px-3 > .form-group:nth-child(1) > .form-control')
+	await page.click('.modal-content > .modal-body > .px-3 > .form-group:nth-child(1) > .form-control')
+	await delay(DELAY);
+
+	await page.waitForSelector('.modal-content > .modal-body > .px-3 > .form-group:nth-child(2) > .form-control')
+	await page.click('.modal-content > .modal-body > .px-3 > .form-group:nth-child(2) > .form-control')
+	await delay(DELAY);
+
+	await page.waitForSelector('.modal-content > .modal-body > .px-3 > .form-group:nth-child(2) > .form-control')
+	await page.click('.modal-content > .modal-body > .px-3 > .form-group:nth-child(2) > .form-control')
+	await delay(DELAY);
+
+	await page.waitForSelector('.modal-dialog > .modal-content > .modal-body > .px-3 > .form-check')
+	await page.click('.modal-dialog > .modal-content > .modal-body > .px-3 > .form-check')
+	await delay(DELAY);
+
+	await page.waitForSelector('#userModal > .modal-dialog > .modal-content > .modal-footer > .dropdown-item:nth-child(1)')
+	await page.click('#userModal > .modal-dialog > .modal-content > .modal-footer > .dropdown-item:nth-child(1)')
+	await delay(DELAY);
+
+	await page.waitForSelector('#userModal > .modal-dialog > .modal-content > .modal-footer > .dropdown-item:nth-child(1)')
+	await page.click('#userModal > .modal-dialog > .modal-content > .modal-footer > .dropdown-item:nth-child(1)')
+	await delay(DELAY);
+
+	await page.waitForSelector('.modal-dialog > .modal-content > .modal-body > .close > span')
+	await page.click('.modal-dialog > .modal-content > .modal-body > .close > span')
+	await delay(DELAY);
+
+	await page.close();
+}
+
+export async function mobile1(browser) {
+	const page = await browser.newPage()
+
+	await page.goto('http://contentsquare-presales-ecomm-web.s3-website-us-west-2.amazonaws.com/')
+
+	await page.setViewport({ width: 404, height: 875 })
+
+	await page.waitForSelector('div > .carousel > #heroControls > .carousel-indicators > .active')
+	await page.click('div > .carousel > #heroControls > .carousel-indicators > .active')
+	await delay(DELAY);
+
+	await page.waitForSelector('#app > div > .carousel > #heroControls > .carousel-indicators')
+	await page.click('#app > div > .carousel > #heroControls > .carousel-indicators')
+	await delay(DELAY);
+
+	await page.waitForSelector('#app > .container-flex > .navbar > .user > h5')
+	await page.click('#app > .container-flex > .navbar > .user > h5')
+	await delay(DELAY);
+
+	await page.waitForSelector('#userModal > .modal-dialog > .modal-content > .modal-footer > .dropdown-item:nth-child(1)')
+	await page.click('#userModal > .modal-dialog > .modal-content > .modal-footer > .dropdown-item:nth-child(1)')
+	await delay(DELAY);
+
+	await page.waitForSelector('.modal-open > #app #userModal')
+	await page.click('.modal-open > #app #userModal')
+	await delay(DELAY);
+
+	await page.waitForSelector('.col-3:nth-child(2) > .footer-widget > .list-unstyled > a:nth-child(3) > li')
+	await page.click('.col-3:nth-child(2) > .footer-widget > .list-unstyled > a:nth-child(3) > li')
+	await delay(DELAY);
+
+	await page.waitForSelector('div > .row > .col-6:nth-child(2) > .card > .card-body')
+	await page.click('div > .row > .col-6:nth-child(2) > .card > .card-body')
+	await delay(DELAY);
+
+	await page.waitForSelector('.col-6:nth-child(2) > .card > .overlay > a > .btn')
+	await page.click('.col-6:nth-child(2) > .card > .overlay > a > .btn')
+	await delay(DELAY);
+
+	await page.waitForSelector('div > .row > .breadcrumb > .breadcrumb-item:nth-child(2) > a')
+	await page.click('div > .row > .breadcrumb > .breadcrumb-item:nth-child(2) > a')
+	await delay(DELAY);
+
+	await page.waitForSelector('.row #MenuLink')
+	await page.click('.row #MenuLink')
+	await delay(DELAY);
+
+	await page.waitForSelector('.row > .view-button > .dropdown > .dropdown-menu > .dropdown-item:nth-child(3)')
+	await page.click('.row > .view-button > .dropdown > .dropdown-menu > .dropdown-item:nth-child(3)')
+	await delay(DELAY);
+
+	await page.waitForSelector('div > .row > .col-6:nth-child(2) > .card > .card-body')
+	await page.click('div > .row > .col-6:nth-child(2) > .card > .card-body')
+	await delay(DELAY);
+
+	await page.waitForSelector('.col-6:nth-child(2) > .card > .overlay > a > .btn')
+	await page.click('.col-6:nth-child(2) > .card > .overlay > a > .btn')
+	await delay(DELAY);
+
+	await page.waitForSelector('div > .row > .col6 > .info > .add-to-cart-button')
+	await page.click('div > .row > .col6 > .info > .add-to-cart-button')
+	await delay(DELAY);
+
+	await page.waitForSelector('.row > .col6 > .info > .control > .increment-button')
+	await page.click('.row > .col6 > .info > .control > .increment-button')
+	await delay(DELAY);
+
+	await page.waitForSelector('.row > .col6 > .info > .control > .increment-button')
+	await page.click('.row > .col6 > .info > .control > .increment-button')
+	await delay(DELAY);
+
+	await page.waitForSelector('.row > .col6 > .info > .control > .increment-button')
+	await page.click('.row > .col6 > .info > .control > .increment-button')
+	await delay(DELAY);
+
+	await page.waitForSelector('.row > .col6 > .info > .control > .increment-button')
+	await page.click('.row > .col6 > .info > .control > .increment-button')
+	await delay(DELAY);
+
+	await page.waitForSelector('div > .row > .col6 > .info > .add-to-cart-button')
+	await page.click('div > .row > .col6 > .info > .add-to-cart-button')
+	await delay(DELAY);
+
+	await page.waitForSelector('.container > div > .more > .col4:nth-child(2) > h6')
+	await page.click('.container > div > .more > .col4:nth-child(2) > h6')
+	await delay(DELAY);
+
+	await page.waitForSelector('.container > div > .more > .col4:nth-child(3) > h6')
+	await page.click('.container > div > .more > .col4:nth-child(3) > h6')
+	await delay(DELAY);
+
+	await page.close();
+}
